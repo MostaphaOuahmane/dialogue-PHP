39bb3510965b43a684bc02a3477608e3427a776c
MostaphaOuahmane <87656171+MostaphaOuahmane@users.noreply.github.com>
2022-01-11 11:13:24 +0100

    dvdvdfv

--- dialogue/update.php
+++ dialogue/update.php
@@ -40,13 +40,15 @@ $pdoDIA = new PDO(
 
         <?php if (isset($_GET['id_commentaires'])) : ?>
             <?php $id_commentaires = $_GET['id_commentaires']; ?>
-            <div class="alert alert-danger d-flex align-items-center" role="alert">
-                <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Danger:">
-                    <use xlink:href="#exclamation-triangle-fill" />
-                </svg>
-                <div>
-                <p>You are updating the idea #<?php echo $id_commentaires; ?></p>
-                </div>
+            <div id="dummy"> 
+                    <div  class="alert alert-primary d-flex align-items-center" role="alert">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-exclamation-triangle-fill flex-shrink-0 me-2" viewBox="0 0 16 16" role="img" aria-label="Warning:">
+                            <path d="M8.982 1.566a1.13 1.13 0 0 0-1.96 0L.165 13.233c-.457.778.091 1.767.98 1.767h13.713c.889 0 1.438-.99.98-1.767L8.982 1.566zM8 5c.535 0 .954.462.9.995l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 5.995A.905.905 0 0 1 8 5zm.002 6a1 1 0 1 1 0 2 1 1 0 0 1 0-2z" />
+                        </svg>
+                        <p>You are updating the idea #<?php echo $id_commentaires; ?></p>
+
+                        
+                    </div>
             </div>
             <br>
             <hr>
@@ -124,7 +126,7 @@ $pdoDIA = new PDO(
                             <?php elseif ($key == 'message') : ?>
                                 <div class="mt-3">
                                     <label for="confmdp"><i class="bi bi-envelope"></i></label>
-                                    <textarea <?php echo $key; ?> name="message" value="<?php echo $value; ?>" id="message" class="form-control" placeholder="votre message"></textarea>
+                                    <textarea <?php echo $key; ?> name="message"  id="message" class="form-control" placeholder="votre message"><?php echo $value; ?></textarea>
                                 </div>
 
                             <?php else : ?>
@@ -133,14 +135,15 @@ $pdoDIA = new PDO(
                         " id_commentaires="<?php echo $key; ?>" <?php if ($key == 'id_commentaires') {
                                                                     echo 'readonly';
                                                                 } ?> <?php if ($key == 'date_enregistrement') {
-                                                        echo 'readonly';
-                                                    } ?>>
+                                                                            echo 'readonly';
+                                                                        } ?>>
 
                             <?php endif; ?>
                             <br>
                             <br>
                         <?php endforeach; ?>
-                        <button onclick="f1()" class="btn btn-outline-secondary bg-success" id="update" type="submit" name="submit" value="Update your idea"> Update your idea</button>
+                        <button onclick="
+                        removedummy(); " class="btn btn-outline-secondary bg-success" id="update" type="submit" name="submit" value="Update your idea"> Update your idea</button>
                     </form>
                 </div>
             </div>
@@ -148,10 +151,21 @@ $pdoDIA = new PDO(
         <?php endif; ?>
 
     </div>
+    <!-- remove avec php non function -->
+    <!-- <script>
+        function removedummy() {
+            const myDiv =document.getElementById("dummy");
+            const parent = myDiv.parentNode;
+            parent.removeChild(myDiv);
+            console.log("should have removed");
+        }
+    </script> -->
+    
+
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
     </script>
-    <script src="app.js"></script>
+    <!-- <script src="dialogue.js"></script> -->
 </body>
 
 </html>
